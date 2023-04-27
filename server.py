d261d4499460eef8043e46e3488d73571f4718ce
MylesMburu <ngicumyles@gmail.com>
2023-04-27 19:14:06 +0300

    day 2

--- server.py
+++ server.py
@@ -12,14 +12,26 @@ def hello_world():
 def sms_callback():
     print(request.method)
     print(request.form)
-    response(request.form["from"], "Yes I have recieved you message")
+    from_number = request.form["from"]
+    incoming_message = request.form["text"].strip().lower() #recieves users text and converts it to lowercase
+
+    if "option1" in incoming_message:
+        message = "You have chosen Option 1. Here is the information you requested."
+    elif "option2" in incoming_message:
+        message = "You have chosen Option 2. Here is the information you requested."
+    elif "option3" in incoming_message:
+        message = "You have chosen Option 3. Here is the information you requested."
+    else:
+        message = "Please send a valid option. Available options: Option1, Option2, Option3"
+
+    send_message(from_number, message)
     return "Success", 201
 
 SANDBOX_API_KEY ="6472b4a2614c40222d035bd83f52c576376441eb2c2adf00fe90276b9ec9c709"
 
 def response(recipient_phone_number, message):
     response = requests.post(
-        "https://api.africastalking.com/version1/messaging",
+        "https://api.sandbox.africastalking.com/version1/messaging",
         data={
             'username': 'sandbox',
             'to': recipient_phone_number,
@@ -33,4 +45,15 @@ def response(recipient_phone_number, message):
         }
     )
     return response
-    
+
+def send_message(recipient_phone_number, message):
+    resp = response(recipient_phone_number, message)
+    if resp.status_code == 201:
+        print("Message sent successfully")
+        print(resp.json())
+    else:
+        print(f"Failed to send message. Status code: {resp.status_code}")
+        print(resp.text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
